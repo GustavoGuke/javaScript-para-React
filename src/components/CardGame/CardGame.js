6867fa204968f28b4bb04a2341f34c3b403523be
gusta <gustavo.athenas13@gmail.com>
2021-07-10 16:00:42 -0300

    curso youtube javaScript para React

---
+++ src/components/CardGame/CardGame.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="src/components/images/alura-pixel.png" alt="logo alura">
+        </article>
+    `
+}
+export default CardGame;
